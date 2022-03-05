2aa759a653ebd5331ab3a814ce27299344206feb
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-03-05 23:09:43 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -27,7 +27,7 @@ class Presets(object):
     NOT_AUTH_TXT = "<b>Error : </b>\n\n<code>You are not Authorized to use this bot.</code>"
     DEFAULT_TITLE = "Inline Tube Mate Repository"
     DEFAULT_THUMB_URL = "https://image.flaticon.com/icons/png/512/25/25231.png"
-    DEFAULT_LINK = "https://t.me/All_Source_Code
+    DEFAULT_LINK = "https://t.me/All_Source_Code"
     DEFAULT_DESCRIPTION = "Link: FRIENDS | GitHub"
     DEV_TITLE = "Developer Information"
     DEV_THUMB_URL = "https://freepikpsd.com/media/2019/10/software-developer-icon-png-2-Transparent-Images.png"
